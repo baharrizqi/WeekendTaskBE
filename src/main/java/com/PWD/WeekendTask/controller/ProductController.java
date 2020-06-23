d0f70bb67aff42c99599998e758de6d2ef574ae3
Muhammad Bahar Rizqi <MR40201X@cimbniaga.co.id>
2020-06-23 15:39:25 +0700

    CRUD Product

--- src/main/java/com/PWD/WeekendTask/controller/ProductController.java
+++ src/main/java/com/PWD/WeekendTask/controller/ProductController.java
@@ -1,11 +1,13 @@
 package com.PWD.WeekendTask.controller;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.Date;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
@@ -15,9 +17,11 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -41,7 +45,7 @@ public class ProductController {
 	@Autowired
 	private ProductRepo productRepo;
 	
-	@GetMapping
+	@GetMapping("/readProduct")
 	public Iterable<Product> getAllProduct(){
 		return productRepo.findAll();
 	}
@@ -50,12 +54,18 @@ public class ProductController {
 //	public Product addProduct(@RequestBody Product product) {
 //		return productRepo.save(product);
 //	}
+	
+	@GetMapping("/readProduct/{id}")
+	public Optional<Product> getProductById(@PathVariable int id) {
+		return productRepo.findById(id);
+	}
+
 	@PostMapping("/createProduct")
-	public String uploadFile(@RequestParam("file") MultipartFile file) throws JsonMappingException, JsonProcessingException {
+	public String uploadFile(@RequestParam("file") MultipartFile file,@RequestParam("productData")String productString) throws JsonMappingException, JsonProcessingException {
 		Date date = new Date();
 		
-//		Product product = new ObjectMapper().readValue(userString, Product.class );
-//		System.out.println("PRODUKNAME: "+ product.getProductName());
+		Product product = new ObjectMapper().readValue(productString, Product.class );
+		System.out.println("PRODUKNAME: "+ product.getProductName());
 		
 		String fileExtension = file.getContentType().split("/")[1];
 		System.out.println(fileExtension);
@@ -72,8 +82,8 @@ public class ProductController {
 		}
 		String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath().path("/products/download/").path(fileName).toUriString();
 		
-//		product.setImageProduct(fileDownloadUri);
-//		productRepo.save(product);
+		product.setImageProduct(fileDownloadUri);
+		productRepo.save(product);
 		
 		return fileDownloadUri;
 	}
@@ -93,5 +103,33 @@ public class ProductController {
 		
 		return ResponseEntity.ok().contentType(MediaType.parseMediaType("application/octet-stream")).header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\""+ resource.getFilename()+ "\"").body(resource);
 	}
+	@DeleteMapping("/delete/{id}")
+	public void deleteProductById(@PathVariable int id) {
+		Product findProduct = productRepo.findById(id).get();
+		
+		if(findProduct.toString()== "Optional.empty")
+			throw new RuntimeException("Product Not Found");
+		
+		productRepo.deleteById(id);
+	}
 	
+	@PutMapping("/editProduct/{id}")
+	public String editProduct(@PathVariable int id,@RequestParam("file") MultipartFile file, @RequestParam("editData") String productString) throws JsonMappingException, JsonProcessingException {
+		Product findProduct = productRepo.findById(id).get();
+		findProduct = new ObjectMapper().readValue(productString, Product.class);
+		Date date = new Date();
+		String fileExtension = file.getContentType().split("/")[1];
+		String newFileName = "PROD-" + date.getTime() + "." + fileExtension;
+		String fileName = StringUtils.cleanPath(newFileName);
+		Path path = Paths.get(StringUtils.cleanPath(uploadPath) + fileName);
+		try {
+			Files.copy(file.getInputStream(), path,StandardCopyOption.REPLACE_EXISTING);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath().path("/products/download/").path(fileName).toUriString();
+		findProduct.setImageProduct(fileDownloadUri);
+		productRepo.save(findProduct);
+		return fileDownloadUri;
+	}
 }
